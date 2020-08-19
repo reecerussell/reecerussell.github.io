034692f3e11274455c42cee4af986fdb2a1e6cbf
Reece Russell <reece@prominentmedia.com>
2020-08-19 15:41:55 +0100

    Updates

---
+++ precache-manifest.71de589c9da6a0298d515cb4d0f97eee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e837f874b5429a1e4cf2743078c821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd84056c4d532fc18970",
+    "url": "/static/css/main.4a106e3d.chunk.css"
+  },
+  {
+    "revision": "fe2160127e93488886a4",
+    "url": "/static/js/2.6d126f0a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6d126f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a0a42a677c4fb42f7f",
+    "url": "/static/js/3.950e6882.chunk.js"
+  },
+  {
+    "revision": "9d97cdad62ca84a003d6",
+    "url": "/static/js/4.271b7dd4.chunk.js"
+  },
+  {
+    "revision": "cd84056c4d532fc18970",
+    "url": "/static/js/main.2f941206.chunk.js"
+  },
+  {
+    "revision": "32c511d22e7cce1ce5b2",
+    "url": "/static/js/runtime-main.e90827f4.js"
+  },
+  {
+    "revision": "0e79ce5449641fbb2efb853f9af3e582",
+    "url": "/static/media/profile.0e79ce54.JPG"
+  }
+]);
